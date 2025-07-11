b74468f8168aa40a08ec12030ccee3e943e9f28d
Kevin Andres Ballen <kballen@medifarma.com.pe>
2025-07-11 14:31:10 -0500

    actualización

---
+++ availability/condition/coursecompleted/tests/behat_test.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Unit tests for the behat coursecompleted condition.
+ *
+ * @package   availability_coursecompleted
+ * @copyright iplusacademy (www.iplusacademy.org)
+ * @author    Renaat Debleu <info@eWallah.net>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+use PHPUnit\Framework\Attributes\CoversClass;
+
+/**
+ * Unit tests for the behat coursecompleted condition.
+ *
+ * @package   availability_coursecompleted
+ * @copyright iplusacademy (www.iplusacademy.org)
+ * @author    Renaat Debleu <info@eWallah.net>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+#[CoversClass(\behat_availability_coursecompleted::class)]
+final class behat_test extends \advanced_testcase {
+    /**
+     * Test behat funcs
+     */
+    public function test_behat(): void {
+        global $CFG;
+        require_once($CFG->dirroot . '/availability/condition/coursecompleted/tests/behat/behat_availability_coursecompleted.php');
+        $class = new \behat_availability_coursecompleted();
+        $this->resetAfterTest();
+        $this->setAdminUser();
+        $CFG->enablecompletion = true;
+        $CFG->enableavailability = true;
+        set_config('enableavailability', true);
+        $dg = $this->getDataGenerator();
+        $course = $dg->create_course(['enablecompletion' => 1]);
+        $user = $dg->create_user();
+        $class->i_mark_course_completed_for_user($course->fullname, $user->username);
+        $this->expectExceptionMessage("A user with username 'otheruser' does not exist");
+        $class->i_mark_course_completed_for_user($course->fullname, 'otheruser');
+    }
+}
---
+++ config.php
@@ -0,0 +1,32 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'mysqli';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle_db';
+$CFG->dbuser    = 'admin';
+$CFG->dbpass    = 'Az1007admin?';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => 3306,
+  'dbsocket' => '',
+  'dbcollation' => 'utf8mb4_unicode_ci',
+);
+
+$CFG->wwwroot   = 'http://localhost/moodle-latest-500/moodle';
+$CFG->dataroot  = 'C:\\laragon\\moodledata';
+$CFG->admin     = 'admin';
+$CFG->debug = 0;
+$CFG->debugdisplay = false;
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
